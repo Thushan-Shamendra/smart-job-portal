86e1d88d8fe044a720905a050beb9ae180c657e9
Thushan-Shamendra <shamendrat@gmail.com>
2026-06-24 01:36:41 +0530

    Connect Profile Routes in server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import jobRoutes from "./routes/jobRoutes.js";
 import applicationRoutes from "./routes/applicationRoutes.js";
+import profileRoutes from "./routes/profileRoutes.js";
 
 dotenv.config();
 
@@ -37,6 +38,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/jobs", jobRoutes);
 
 app.use("/api/applications", applicationRoutes);
+
+app.use("/api/profile", profileRoutes);
 
 const PORT = process.env.PORT || 5000;
 
